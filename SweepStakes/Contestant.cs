b28e2a1de53aec3d25deee2c7a80fea454781832
Robert Smidt <rsmidt909@hotmail.com>
2019-05-20 15:25:04 -0500

    Created repo, laid bones from userStories

---
+++ SweepStakes/Contestant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class Contestant
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public int registrationNumber { get; set; }
+    }
+}
---
+++ SweepStakes/Sweepstakes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class Sweepstakes
+    {
+        public static Sweepstakes(string name)
+        {
+
+        }
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+
+        public string PickWinner()
+        {
+            return
+        }
+
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+
+    }
+}
